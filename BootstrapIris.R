af3573021225868faf14a0cc453b861204a869c1
fordeia <157367221+fordeia@users.noreply.github.com>
2025-04-01 20:15:32 -0400

    Update BootstrapIris.R

--- BootstrapIris.R
+++ BootstrapIris.R
@@ -4,3 +4,10 @@ iris.rg <- randomForest(Sepal.Length ~ ., data=iris, importance=TRUE,
                         proximity=TRUE)
 
 sqrt(sum((iris.rg$predicted - iris$Sepal.Length)^2) / nrow(iris))
+
+
+bootdataIris=iris[sample(nrow(iris), 1000, replace=TRUE), ]
+
+head(bootdataIris)
+nrow(bootdataIris
+str(bootdataIris)
